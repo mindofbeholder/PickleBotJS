581df855d5bb9c683ce9daba9da16c797bbc6167
mindofbeholder <nic@nicolasgreen.com>
2018-05-23 21:25:08 -0400

    Added bot typing effect in channel

--- commands/hots-info.js
+++ commands/hots-info.js
@@ -23,11 +23,11 @@ module.exports = {
             }
         };
 
-        var mmrMessage = "";
         if (!usernameRegEx.test(args[0])) {
             message.reply("that is an invalid battletag.");
             return;
         } else {
+            message.channel.startTyping();
             let battletag = args[0].replace("#","_");
             url = "".concat("https://api.hotslogs.com/Public/Players/1/",battletag);
         }
@@ -35,17 +35,16 @@ module.exports = {
         .then(res => res.json())
         .then(json => {
             if (json === null) {
+                message.channel.stopTyping();
                 message.reply("no player data found with the name " + args[0]);
                 return;
             }
             fields = mmrEmbed.embed.fields;
             fields.push({name: "Player",value: json.Name});
             for (i = 0; i < json.LeaderboardRankings.length; i++) {
-                fields.push({name: json.LeaderboardRankings[i].GameMode, value: json.LeaderboardRankings[i].CurrentMMR, inline: true })
-                //fields.push("name": "${json.LeaderboardRankings[i].GameMode}", "value": "${json.LeaderboardRankings[i].CurrentMMR}", "inline": true);
-                //mmrMessage = mmrMessage.concat("\n**Game Mode: ** ",json.LeaderboardRankings[i].GameMode);
-                //mmrMessage = mmrMessage.concat("\n**MMR: ** ",json.LeaderboardRankings[i].CurrentMMR,"\n");
+                fields.push({name: json.LeaderboardRankings[i].GameMode, value: json.LeaderboardRankings[i].CurrentMMR, inline: true });
             }
+            message.channel.stopTyping();
             message.reply(mmrEmbed);
         });
     },
