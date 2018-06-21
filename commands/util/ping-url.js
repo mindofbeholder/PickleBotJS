166ef0d0512da58392c97d0f97cedf16c4b166cb
mindofbeholder <nic@nicolasgreen.com>
2018-06-20 21:10:38 -0400

    FEAT: Complete feature

--- commands/util/ping-url.js
+++ commands/util/ping-url.js
@@ -1,5 +1,8 @@
 const commando = require('discord.js-commando');
 const oneLine = require('common-tags').oneLine;
+const Discord = require('discord.js');
+var screenshot = require('snapshot-stream');
+const fs = require('fs');
 
 module.exports = class PingURL extends commando.Command {
     constructor(client) {
@@ -28,6 +31,41 @@ module.exports = class PingURL extends commando.Command {
     }
 
     async run(message, args) { // jshint ignore:line
-        message.channel.send('test');
+        let finalURL = "";
+
+        if (args.URL.indexOf('http://') === -1 & args.URL.indexOf('https://') === -1) {
+            finalURL = `https://${args.URL}`;
+        } else if (args.URL.indexOf('http://') !== -1) {
+            finalURL = `https://${args.URL.substr(7)}`;
+        } else if (args.URL.indexOf('https://') !== -1) {
+            finalURL = `https://${args.URL.substr(8)}`;
+        }
+        
+        message.channel.send(`Grabbing ${args.URL} now. Please hold.`);
+
+        try {
+            var stream = screenshot(finalURL, '1024x768', {crop: true});
+            await stream.pipe(fs.createWriteStream(`images/${message.author.id}.png`)) // jshint ignore:line
+            .on('finish', () => {
+                message.channel.send({file: `images/${message.author.id}.png`});
+            })
+            .on('error', (err) => {
+                message.channel.send(err);
+            });
+        } catch (err) {
+            console.log(err);
+            message.channel.send('There was an error. Try again!');
+        }
+        
+        
+        // const picEmbed = new Discord.RichEmbed()
+        // .setColor('green')
+        // .setTitle(args.URL)
+        // .attachFile({file: `image.png`});
+
+        
+
+        
+
     }
 };
